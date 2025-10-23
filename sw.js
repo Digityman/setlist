d44965a281a58748107829cbb7ba4b27ef5a49ec
Digityman <75377660+Digityman@users.noreply.github.com>
2025-10-23 05:37:56 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// Minimal SW (no no-op fetch handler). Safe to keep or remove.
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-// No 'fetch' listener on purpose to avoid the no-op warning.
